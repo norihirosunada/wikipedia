70276e9d9a2f699def5e11d18c4c13789ee3d994
norihirosunada <norisuna1201@gmail.com>
2020-02-06 06:31:47 +0000

    003 done

--- app.rb
+++ app.rb
@@ -1,3 +1,21 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+require 'net/http'
+require 'uri'
+
+get '/' do 
+    base_url = 'http://wikipedia.simpleapi.net/api?output=html&keyword='
+    if params[:keyword]
+        keyword = URI.escape(params[:keyword])
+        url = URI.parse(base_url+keyword)
+        if Net::HTTP.get(url) != ""
+            @result = Net::HTTP.get(url).force_encoding("utf-8")
+        else
+            @result = "params[:keyword]がありません。"
+        end
+    end
+    erb :index
+end
+
